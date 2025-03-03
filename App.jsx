b1df8732d7763248f5bbae0e7b0ecb91bc2be3f1
Ануар <seisenbaianuarr@gmail.com>
2025-03-03 21:46:33 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "#f5f5f5" }}>
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProfileCard.css"; 
+
+const ProfileCard = () => {
+  return (
+    <div className="profile-card">
+      <h2 className="name">my name is: SEISENBAI ANUAR</h2>
+      <p className="job">BEZ RABOTNIY</p>
+      <p className="city">CONTRY: KZ</p>
+    </div>
+  );
+};
+
+export default ProfileCard;
